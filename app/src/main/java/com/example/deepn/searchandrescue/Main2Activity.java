2cb377030c6a69f7fae72538361baed44c8a27f4
sakhayadeep <sakhayadeepnath@gmail.com>
2018-10-14 00:18:22 +0530

    design finish

--- app/src/main/java/com/example/deepn/searchandrescue/Main2Activity.java
+++ app/src/main/java/com/example/deepn/searchandrescue/Main2Activity.java
@@ -5,17 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
-    public void startSearch(View view) {
-        Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
-        MainActivity.this.startActivity(myIntent);
+    public void start3screen(View view) {
+        Intent myIntent = new Intent(Main2Activity.this, Main3Activity.class);
+        Main2Activity.this.startActivity(myIntent);
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main2);
     }
 }
